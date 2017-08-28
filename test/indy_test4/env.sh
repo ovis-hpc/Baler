da839737ae18cf0f3e3336c6c0875e82f802a40f
Narate Taerat <narate@ogc.us>
2017-08-28 11:01:54 -0500

    indy_test4 - update env.sh
    Update indy_test4/env.sh to support the case that sos and baler are
    installed into different prefixes.

--- test/indy_test4/env.sh
+++ test/indy_test4/env.sh
@@ -3,8 +3,13 @@
 # Modify your ENV VARs so that baler programs can run
 # Don't forget to add dependencies (like sos3) into the library path
 
-PREFIX=$HOME/opt/baler4
-export PATH="$PREFIX/bin:$PATH"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export ZAP_LIBPATH=$PREFIX/lib/ovis-lib
-export PYTHONPATH=$PREFIX/lib/python2.7/site-packages
+BALER_PREFIX=$HOME/opt/baler
+SOS_PREFIX=$HOME/opt/sos
+export PATH="$BALER_PREFIX/bin:$PATH"
+LD_LIBRARY_PATH="$SOS_PREFIX/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$BALER_PREFIX/lib:$LD_LIBRARY_PATH"
+export ZAP_LIBPATH=$BALER_PREFIX/lib/ovis-lib
+PYTHONPATH=$BALER_PREFIX/lib/python2.7/site-packages
+PYTHONPATH=$PYTHONPATH:$SOS_PREFIX/lib/python2.7/site-packages
+export PYTHONPATH
+export BSTORE_PLUGIN_PATH=$BALER_PREFIX/lib
