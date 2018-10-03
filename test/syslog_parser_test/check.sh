d89fe496e3c0e8e3a43f5b9e3849da880dc9d18b
Narate Taerat <narate@ogc.us>
2018-10-03 08:59:28 -0500

    Fix check script return code in syslog_parser_test

--- test/syslog_parser_test/check.sh
+++ test/syslog_parser_test/check.sh
@@ -2,6 +2,7 @@
 
 source ./common.sh
 
+ECODE=0
 # Put query test cases here
 for X in check-scripts/check*; do
 	if [ ! -f "$X" ] || [ ! -x "$X" ]; then
@@ -11,7 +12,10 @@ for X in check-scripts/check*; do
 	time -p ./$X
 	if (($?)); then
 		__err "................... $X ${BLD}${RED}failed${NC}"
+		ECODE=-1
 	else
 		__info "................... $X ${BLD}${GRN}success${NC}"
 	fi
 done
+
+exit $ECODE
